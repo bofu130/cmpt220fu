55b0b12d87da016e6864bf496d21713c74fbaabc
bofu130 <bo.fu1@marist.edu>
2017-02-19 12:29:25 -0500

    Add header.

--- prj/0/Driver_prj0.java
+++ prj/0/Driver_prj0.java
@@ -1,3 +1,14 @@
+ /**
+   * file: Driver_prj0.java
+   * author: Bo Fu
+   * course: CMPT 220
+   * assignment: project 0
+   * due date: January / 24 / 2017
+   * version: 1.0
+   * 
+   * This file contains the declaration of the 
+   * SinglyLinkedList abstract data type.
+   */
 public class Driver_prj0 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
