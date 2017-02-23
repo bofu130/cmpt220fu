20dfbaa3cde75467bb5f4b3db0683a6b42436bcb
bofu130 <bo.fu1@marist.edu>
2017-02-22 20:57:04 -0500

    can work, fell in kattis.

--- lab/3/Driver_lab3.java
+++ lab/3/Driver_lab3.java
@@ -4,17 +4,30 @@
 *course: CMPT220
 *assignment: lab_3
 *due date: Feb 21
-*version: 1.0
+*version: 1.2
 */
+import java.util.Scanner;
 public class Driver_lab3 {
   public static void main(String[] args) {
+	boolean isTrue = true;
+	while (isTrue == true) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter x1, y1, x2, y2, p: ");
     double x1, x2, y1, y2, p;
-	x1 = 1.0;
-	x2 = 1.0;
-	y1 = 2.0;
-	y2 = 2.0;
-	p = 2.0;
-    double result = Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p), 1/p);
-    System.out.printf("%.4f",result);
+	x1 = input.nextDouble();
+	x2 = input.nextDouble();
+	y1 = input.nextDouble();
+	y2 = input.nextDouble();
+	p = input.nextDouble();
+	if (x1 == 0) {
+		break;
+	}
+    double result = Math.pow(Math.pow(Math.abs(x1 - y1), p) + Math.pow(Math.abs(x2 - y2), p), 1/p);
+    System.out.printf("%.10f",result);
+	  //isTrue = true;
+	  //return isTrue;
+	
+	
+	}
+	}
   } 
-}
