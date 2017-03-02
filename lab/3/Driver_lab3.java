98fe6960290d9d86ad502b255d444dc6207a48e7
bofu130 <bo.fu1@marist.edu>
2017-03-01 20:26:45 -0500

    Pass Kattis.

--- lab/3/Driver_lab3.java
+++ lab/3/Driver_lab3.java
@@ -4,26 +4,27 @@
 *course: CMPT220
 *assignment: lab_3
 *due date: Feb 21
-*version: 1.2
+*version: 1.3
 */
 import java.util.Scanner;
 public class Driver_lab3 {
   public static void main(String[] args) {
-	boolean isTrue = true;
-	while (isTrue == true) {
 	Scanner input = new Scanner(System.in);
-	System.out.print("Enter x1, y1, x2, y2, p: ");
+  while (true) {
+	
+	//System.out.print("Enter x1, y1, x2, y2, p: ");
     double x1, x2, y1, y2, p;
-	x1 = input.nextDouble();
-	x2 = input.nextDouble();
-	y1 = input.nextDouble();
-	y2 = input.nextDouble();
-	p = input.nextDouble();
-	if (x1 == 0) {
-		break;
+	  x1 = input.nextDouble();
+	  if (x1 == 0) {
+	    break;
 	}
-    double result = Math.pow(Math.pow(Math.abs(x1 - y1), p) + Math.pow(Math.abs(x2 - y2), p), 1/p);
-    System.out.printf("%.10f",result);
+	  y1 = input.nextDouble();
+	  x2 = input.nextDouble();
+	  y2 = input.nextDouble();
+	  p = input.nextDouble();
+
+      double result = Math.pow((Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p)), 1/p);
+      System.out.printf("%.10f\n",result);
 	  //isTrue = true;
 	  //return isTrue;
 	
