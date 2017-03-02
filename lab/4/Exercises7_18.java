31a71d5f8cee56ad9068632ff369a74496b849c9
bofu130 <bo.fu1@marist.edu>
2017-03-02 15:09:32 -0500

    add .length to it

--- lab/4/Exercises7_18.java
+++ lab/4/Exercises7_18.java
@@ -8,16 +8,15 @@
 */
 import java.util.Scanner;
 public class Exercises7_18 {
-  //public static double[] myList = new double[10];
   public static void main (String[] args) {
     double[] myList = new double[10];
     Scanner input = new Scanner(System.in);
 	  System.out.print("Enter 10 numbers: ");
-	  for (int i = 0; i < 10; i++) {
+	  for (int i = 0; i < myList.length; i++) {
       myList[i] = input.nextDouble();
 	  }
     bubble(myList);
-    for (int k  = 0; k < 10; k++) {
+    for (int k  = 0; k < myList.length; k++) {
       System.out.print(myList[k] + " ");
 	  }	
   }
